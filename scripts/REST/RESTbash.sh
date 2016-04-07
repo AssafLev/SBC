249fc327d21e95669d84ae36263546ef090a08ca
alon-strik <alons@gigaspaces.com>
2016-04-07 19:51:06 +0300

    Notes

--- scripts/REST/RESTbash.sh
+++ scripts/REST/RESTbash.sh
@@ -1,31 +1,17 @@
-#!/bin/bash -v
+#!/bin/bash -x
 
 SBCIP=$1
+command=$2
 
 url_basic="http://$SBCIP/api/v1"
 rest_header="Content-Type:application/json "
 get_action="-X GET"
 put_action="-X PUT"
 post_action="-X POST"
+auth_key="-uAdmin:Admin"
 
-curl -v -H \"${rest_header}\" ${get_action} ${url_basic}/status
+curl -v -H ${rest_header} ${auth_key} ${get_action} ${url_basic}/${command}
 
 
 #curl -v -H \"${rest_header}\" ${rest_action} --data "@SetInterfaces-new.json" ${url_basic}/3.5/config/active/traffic_managers/172.30.0.10
-#
-#curl -v -H \"${rest_header}\" ${rest_action} --data "@AddPool.json" ${url_basic}/3.5/config/active/pools/CFY-Pool
-#
-#curl -v -H \"${rest_header}\" ${rest_action} --data "@CreatingTrafficIPGroup.json" ${url_basic}/2.0/config/active/traffic_ip_groups/CFY-TIP
-
-#curl -v -H ${rest_header} ${rest_action} \
-#     --data "@CreateSessionPersistence.json" \
-#     ${url_basic}/2.0/config/active/persistence/Persistence
-
-#curl -v -H ${rest_header} ${rest_action} \
-#     --data "@AssigningSessionPersistence.json" \
-#     ${url_basic}/3.5/config/active/pools/CFY-Pool
-
-#curl -v -H ${rest_header} ${rest_action} \
-#     --data "@CreatingVirtualServer.json" \
-#     ${url_basic}/2.0/config/active/virtual_servers/CFY-VS
 
